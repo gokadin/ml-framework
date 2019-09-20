e25556c2b4155d4abf5d0abfc45bbb5d2457f808
Givi Odikadze <godikadze@GENETEC.COM>
2019-09-20 15:36:05 -0400

    progressing

--- tensor/autograd.go
+++ tensor/autograd.go
@@ -1,27 +1,85 @@
 package tensor
 
-type Autograd struct {
+import "log"
 
+type Autograd struct {
+	cache map[string][]*operation
 }
 
 func NewAutograd() *Autograd {
 	return &Autograd{
-
+		cache: make(map[string][]*operation),
 	}
 }
 
-func (a *Autograd) Gradient(c, f *Tensor) [][]float64 {
-	return [][]float64{{1}}
+func (a *Autograd) Derivative(c, f *Tensor) [][]float64 {
+	return computeDerivative(a.getDerivativeGraph(c, f))
 }
 
-func isolate(c, f *Tensor) *operation {
-	return findComponentOperation(c.operation, f.operation)
+func (a *Autograd) getDerivativeGraph(c, f *Tensor) []*operation {
+	hash := calculateDerivativeHash(c, f)
+	if _, ok := a.cache[hash]; ok {
+		return a.cache[hash]
+	}
+	
+    a.cache[hash] = createDerivativeGraph(c.operation, f.operation)
+    return a.cache[hash]
 }
 
-func findComponentOperation(leaf, root *operation) *operation {
-	for _, child := range root.children {
-		if child.tensor.id == leaf.tensor.id {
-            op := newOperation(operationDifferentiateNone, child.tensor, []*operation{})
+func calculateDerivativeHash(c, f *Tensor) string {
+	return c.id + f.id
+}
+
+func createDerivativeGraph(c, f *operation) []*operation {
+    graph := make([]*operation, 0)
+    
+    current := f
+	for !current.isLeaf() {
+		graph = append(graph, current)
+		for _, child := range current.children {
+			if child.tensor.id == c.tensor.id {
+				current = child
+				break
+			}
 		}
 	}
 }
+
+func findThatSpecialChild(c *operation, children []*operation) int {
+    for i, child := range children {
+        if leafIsInPath(c, child) {
+        	return i
+		}
+	}
+    
+    log.Fatal("no child was special")
+    return 0
+}
+
+func leafIsInPath(c, root *operation) bool {
+    
+}
+
+func findComponentOperation(leaf, root *operation) *operation {
+    var marked *operation
+	for _, child := range root.children {
+		if child.tensor.id == leaf.tensor.id {
+            marked = child
+		}
+	}
+	
+	
+
+	if marked == nil {
+        log.Fatalf("did not find component")
+	}
+}
+
+func optimize(f *operation) *operation {
+	
+}
+
+func computeDerivative(graph []*operation) [][]float64 {
+
+}
+
--- tensor/operation.go
+++ tensor/operation.go
@@ -1,20 +1,20 @@
 package tensor
 
+import "log"
+
 const (
 	operationNone = "operationNone"
 	operationAdd = "operationAdd"
 	operationSub = "operationSub"
-
-	operationDifferentiateNone = "operationDifferentiateNone"
-	operationDifferentiateSub = "operationDifferentiateSub"
-	operationDifferentiateAdd = "operationDifferentiateAdd"
 )
 
 type operation struct {
 	name string
 	tensor *Tensor
+	gradient [][]float64
 	children []*operation
 	metadata []float64
+	isMarked bool
 }
 
 func newOperation(name string, tensor *Tensor, children []*operation, metadata ...float64) *operation {
@@ -28,4 +28,35 @@ func newOperation(name string, tensor *Tensor, children []*operation, metadata .
 
 func (o *operation) isLeaf() bool {
 	return o.name == operationNone
+}
+
+func (o *operation) differentiate() {
+    switch o.name {
+	case operationNone:
+        o.gradient = o.tensor.mat
+		break
+	case operationAdd:
+        o.gradient = findMarkedChild(o).gradient
+		break
+	case operationSub:
+		if o.children[0].isMarked {
+			o.gradient = o.children[0].gradient
+		} else if o.children[1].isMarked {
+			o.gradient = mulScalar(o.children[1].gradient, -1)
+		} else {
+			log.Fatal("could not find a marked child")
+		}
+		break
+	}
+}
+
+func findMarkedChild(operation *operation) *operation {
+    for _, child := range operation.children {
+    	if child.isMarked {
+    		return child
+		}
+	}
+    
+    log.Fatalf("could not find a marked child")
+    return nil
 }
