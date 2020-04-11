f3d745ab1a3c122af1c921d56956e1313b0f55c1
Givi Odikadze <givi.odikadze@gmail.com>
2020-04-11 14:44:10 -0700

    x

--- rl/REINFORCE.go
+++ rl/REINFORCE.go
@@ -2,11 +2,13 @@ package rl
 
 import (
 	"fmt"
+	"github.com/encryptio/alias"
 	"github.com/gokadin/ml-framework/graphics2D"
 	"github.com/gokadin/ml-framework/mat"
 	"github.com/gokadin/ml-framework/models"
 	"github.com/gokadin/ml-framework/modules"
 	"github.com/gokadin/ml-framework/tensor"
+	gym "github.com/openai/gym-http-api/binding-go"
 	"math/rand"
 	"runtime"
 	"time"
@@ -48,17 +50,76 @@ func NewReinforce() *Reinforce {
 	}
 }
 
+const BaseURL = "http://localhost:5000"
+
 func (w *Reinforce) Run() {
 	w.metric.start()
 	w.model = w.buildModel()
-}
 
-func (w *Reinforce) selectAction(qval *tensor.Tensor) int {
-	if rand.Float64() < w.epsilon {
-		return rand.Intn(4)
+	client, err := gym.NewClient(BaseURL)
+	if err != nil {
+		panic(err)
 	}
 
-	return maxIndex(qval.Data().Data())
+	insts, err := client.ListAll()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Started with instances:", insts)
+
+	id, err := client.Create("CartPole-v0")
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close(id)
+
+	actSpace, err := client.ActionSpace(id)
+	must(err)
+	fmt.Printf("Action space: %+v\n", actSpace)
+
+	must(client.StartMonitor(id, "/tmp/cartpole-monitor", false, false, false))
+
+	stateMat := make([]float32, 4)
+	state := tensor.Variable(mat.WithShape(1, 4))
+	pred := w.model.Predict(state)
+	loss := tensor.Neg()
+
+	fmt.Println()
+	fmt.Println("Starting new episode...")
+	obs, err := client.Reset(id)
+	must(err)
+	fmt.Println("First observation:", obs)
+	for {
+		act, err := client.SampleAction(id)
+		must(err)
+
+		// Take the action, getting a new observation, a reward,
+		// and a flag indicating if the episode is done.
+		newObs, rew, done, _, err := client.Step(id, act, false)
+		newState := w.obsToState(newObs.([]float64))
+		must(err)
+		obs = newObs
+		fmt.Println("reward:", rew, " -- observation:", obs)
+		if done {
+			break
+		}
+	}
+
+	must(client.CloseMonitor(id))
+}
+
+func (w *Reinforce) selectAction(probabilities []float64) int {
+	x, _ := alias.New(probabilities)
+	index := x.Gen(rand.New(rand.NewSource(time.Now().UTC().UnixNano())))
+	return int(index)
+}
+
+func (w *Reinforce) obsToState(obs []float64) []float32 {
+	state := make([]float32, len(obs))
+	for i := 0; i < len(obs); i++ {
+		state[i] = float32(obs[i])
+	}
+	return state
 }
 
 func (w *Reinforce) calculateYValue(maxQValue float32, reward int) float32 {
@@ -218,4 +279,10 @@ func (w *Reinforce) addNoise(mat *mat.Mat32f) *mat.Mat32f {
 		return f + rand.Float32() / 10
 	})
 	return mat
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
